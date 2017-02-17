2ded9bf7216d9c8380aa9ffd18abb850b915ab17
Kevin Dunn <kgdunn@gmail.com>
2017-02-17 06:39:52 -0500

    Updated images location

--- peer/settings.py
+++ peer/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = '/images/'
 
 try:
     from local_settings import *
