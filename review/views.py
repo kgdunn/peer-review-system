63ff989ad52ca5276ee0eab2d6845a90d222487f
Kevin Dunn <kgdunn@gmail.com>
2017-03-01 18:38:41 +0100

    Removed depricated module

--- review/views.py
+++ review/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 
 from django.views.decorators.clickjacking import xframe_options_exempt
