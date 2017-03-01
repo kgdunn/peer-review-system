ef2ebb4ad66316546225e1ce2caf31088f104afe
Kevin Dunn <kgdunn@gmail.com>
2017-03-01 16:30:48 +0100

    Clear up field name

--- review/forms.py
+++ review/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class UploadFileForm(forms.Form):
 
-    classlist = forms.FileField(widget=forms.ClearableFileInput(
+    file_submission = forms.FileField(widget=forms.ClearableFileInput(
         attrs={'multiple': False,
                'initial_text': "Please upload your submission"}))
