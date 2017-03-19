75ef82b896962d55e4a50cb7bb90555023a193d6
Kevin Dunn <kgdunn@gmail.com>
2017-03-19 09:40:43 +0100

    Better admin progress tracking

--- review/views.py
+++ review/views.py
@@ -26,6 +26,7 @@ import hashlib
 import numpy as np
 import json
 from random import shuffle
+from collections import defaultdict
 
 # 3rd party imports
 import magic  # used to check if file uploads are of the required type
@@ -573,6 +574,41 @@ def get_related(self, request, learner, ctx_objects, now_time, prior):
                     next_sub.save()
                     r_actuals.append(r_actual)
                     logger.debug('Created r_actual: ' + str(r_actual))
+
+        if learner.role != 'Learn':
+            sub_stats = {}  # r_actual_stats
+            all_ra = RubricActual.objects.filter(rubric_template=r_template)
+
+            # admin_grader_completed: bool (0 or 1 int)
+            # admin_grader_inprogress: bool
+            # learner_grader_completed: int
+            # learner_grader_inprogress: int
+
+            for item in all_ra:
+                key = item.submission
+                if sub_stats.get(key, None) is None:
+                    sub_stats[key] = defaultdict(int)
+                if item.graded_by.role == 'Admin':
+                    if not(sub_stats[key]['admin_grader_inprogress']):
+                        if item.status == 'P':
+                            sub_stats[key]['admin_grader_inprogress'] = 1
+                    if not(sub_stats[key]['admin_grader_completed']):
+                        if item.status == 'C':
+                            sub_stats[key]['admin_grader_completed'] = 1
+
+                if item.graded_by.role == 'Learn':
+                    if item.status in ('P', 'A'):
+                        sub_stats[key]['learner_grader_inprogress'] += 1
+                    if item.status == 'C':
+                        sub_stats[key]['learner_grader_completed'] += 1
+
+
+            # Now that we have statistics for each submission, associate it
+            # back to the r_actuals for the admins:
+
+            for item in r_actuals:
+                key = item.submission
+                item.summary_stats = sub_stats[key]
 
         # Now we have the peer review ojects: ``r_actuals``
         ctx_objects['r_actuals'] = r_actuals
