5ecd598b6a3fd322bc34ca2eff67345b98064fbf
Kevin Dunn <kgdunn@gmail.com>
2017-04-30 10:33:52 +0200

    Changes for production

--- grades/views.py
+++ grades/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Our imports
 from .models import GradeBook, GradeCategory, GradeItem, LearnerGrade
