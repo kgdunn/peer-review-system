6bf3e64d59a0fc680267477cc78dea32e59e76c3
Kevin Dunn <kgdunn@gmail.com>
2017-05-01 21:13:21 +0200

    Formatting the grade display for the learner

--- grades/views.py
+++ grades/views.py
@@ -12,19 +12,113 @@ from review.models import Person
 import io
 import csv
 import six
+import decimal
 import datetime
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
 # Logging
 import logging
 logger = logging.getLogger(__name__)
 
+def get_grade_summary(learner, gradebook):
+    """
+    Get the complete grade breakdown summary for a learner in a given
+    ``gradebook``.
+
+    We also return, as a second output, the grade average (total grade obtained
+    so far in the course)
+    """
+    categories = GradeCategory.objects.filter(gradebook=gradebook)\
+        .order_by('order')
+
+    """
+    How will we represent gradebook to a student? Data structure:
+
+    [(string,decimal,{}), (string,decimal,{}), (string,decimal,{})]
+
+    1. Outer list: holds, in order, the categories
+
+    2. The innner tuple: holds 3 disparate elements: string, decimal, dictionary
+       2a. The catogory name
+       2b. The category weights (the sum of all weights should be 100%)
+       2c. The dictionary* of item(s) within that category.
+
+    3. The inner dictionaries*:
+       3a. Key ('5.02', for example): indicates category 5, item 2
+       3b. Value: Will be an instance of "LearnerGrade" for that item.
+
+    * The dictionaries are sorted, just before returning and returned with:
+        sorted(dict.items())  if PY3,    or    sorted(dict.iteritems()) if PY2
+
+
+    To calculate the student average:
+    * iterate over all the items in dictionary to calculate the score for category
+    * iterate over all categories in  list, with the weight, to get overall average
+    """
+    grades = []
+    total_grade = decimal.Decimal(0.0)
+    for gcat in categories:
+        items = gcat.gradeitem_set.all().order_by('order')
+        if items.count() == 0:
+            # No items?
+            continue
+        elif items.count() == 1:
+            weight = max(gcat.weight, items[0].weight)
+            grade = LearnerGrade.objects.filter(learner=learner, gitem=items[0])
+            if grade.count() == 0:
+                print('How now?')
+                pass
+            else:
+                grades.append((gcat.display_name,
+                                weight*100,
+                                [('0', grade[0])]
+                             ))
+                if grade[0].value:
+                    total_grade += weight * grade[0].value
+            continue
+
+        items_dict = {}
+        item_grades = []
+        item_weights = []
+        category_score = decimal.Decimal(0.0)
+        if gcat.spread_evenly:
+            weight = decimal.Decimal(1/(items.count()+0.0))
+            item_weights = [weight]*items.count()
+
+        for idx, item in enumerate(items):
+            grade = LearnerGrade.objects.filter(learner=learner, gitem=item)
+            if grade.count() == 0:
+                # How do we get to this point?
+                item_grades.append(0.0)
+                pass
+            else:
+                key = gcat.order + item.order/1000.0
+                items_dict[key] = grade[0]
+                item_weights[idx] = max(item_weights[idx], item.weight)
+                if grade[0].value:
+                    category_score += item_weights[idx] * grade[0].value
+
+        # After processing all items
+        total_grade += gcat.weight * category_score
+        LearnerGrade_nt = namedtuple('gradeitem', ['value', 'max_score',
+                                                   'not_graded_yet' ])
+        items_dict[-10000] = LearnerGrade_nt(value=category_score,
+                                             max_score=100,
+                                             not_graded_yet=False)
+        if six.PY2:
+            items_dict = sorted(items_dict.iteritems())
+
+        elif six.PY3:
+            items_dict = sorted(items_dict.items())
+
+        grades.append((gcat.display_name, gcat.weight*100, items_dict))
+
+    return grades, total_grade
+
 def display_grades(learner, course, pr, request):
     """
     Displays the grades to the student here.
     """
-    gradebook = GradeBook.objects.get(course=course)
-    grades = {}
     if learner.role == 'Admin':
         ctx = {'learner': learner,
                'course': course,
@@ -33,29 +127,13 @@ def display_grades(learner, course, pr, request):
         return render(request,
                       'grades/import_grades.html', ctx)
 
-    categories = GradeCategory.objects.filter(gradebook=gradebook)\
-                                                            .order_by('order')
-
-    for gcat in categories:
-        if gcat.gradeitem_set.all().count() == 0:
-            continue
-
-        items = gcat.gradeitem_set.all().order_by('order')
-        for item in items:
-            grade = LearnerGrade.objects.filter(learner=learner, gitem=item)
-            if grade.count() == 0:
-                pass
-            else:
-                key = gcat.order + item.order/1000.0
-                grades[key] = grade[0]
-
-    if six.PY2:
-        ctx = {'grades': sorted(grades.iteritems())}
-    elif six.PY3:
-        ctx = {'grades': sorted(grades.items())}
-
-    ctx['course'] = course
-    ctx['gradebook'] = gradebook
+    gradebook = GradeBook.objects.get(course=course)
+    grades, total_grade = get_grade_summary(learner, gradebook)
+    ctx = {'course': course,
+           'gradebook': gradebook,
+           'grades': grades,
+           'total_grade': total_grade
+           }
 
     return render(request, 'grades/learner_grades.html', ctx)
 
